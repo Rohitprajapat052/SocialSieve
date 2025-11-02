e735e5ea971101da21a2fe766bcf3f6efae281b2
RohitPipalki <114174101+Rohitprajapat052@users.noreply.github.com>
2025-11-03 03:32:05 +0530

    hohoho

--- frontend/frontend/services/api.ts
+++ frontend/frontend/services/api.ts
@@ -10,7 +10,7 @@ import type {
 } from '../types';
 
 // Base API URL - Change this to your backend URL
-const API_URL = import.meta.env.VITE_API_URL;
+VITE_API_URL=https://socialsieve-b.onrender.com
 
 // Create axios instance
 const api = axios.create({
